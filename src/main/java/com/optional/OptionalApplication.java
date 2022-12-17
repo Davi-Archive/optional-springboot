2f09d697beb5b0b364756d0302191dcc28f4b849
d1av <davi4alves@gmail.com>
2022-12-17 01:28:53 -0300

    feat: new non Optional code

--- src/main/java/com/optional/OptionalApplication.java
+++ src/main/java/com/optional/OptionalApplication.java
@@ -1,13 +1,21 @@
 package com.optional;
 
+import com.optional.model.Caminhao;
+import com.optional.model.Motorista;
+import com.optional.repository.Motoristas;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 public class OptionalApplication {
-
 	public static void main(String[] args) {
-		SpringApplication.run(OptionalApplication.class, args);
+		Motoristas motoristas = new Motoristas();
+		Motorista motorista = motoristas.porNome("João");
+
+//		String cobertura =  motorista.getCaminhao().getSeguro().getCobertura();
+		String cobertura = null;
+
+		System.out.println("A cobertura é: "+cobertura);
+
 	}
 
 }
